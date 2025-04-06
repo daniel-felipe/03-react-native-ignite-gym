d1946fdcd478f3cc1ea1aa75188b1e4b0b884048
= <daniell.felipe.2017@gmail.com>
2025-04-06 17:21:27 -0300

    add roboto font

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import { Text, View } from 'react-native'
 
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular,
+  })
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
+      {fontsLoaded ? <Text>Home</Text> : <View />}
     </View>
   )
 }
