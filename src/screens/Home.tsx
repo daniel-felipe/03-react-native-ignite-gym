abd83a02bb66c83d45ec349773d2ee43440340cb
= <daniell.felipe.2017@gmail.com>
2025-04-13 21:35:19 -0300

    list groups on Home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,32 @@
+import { useState } from 'react'
+
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-
-import { VStack } from '@gluestack-ui/themed'
+import { FlatList, VStack } from '@gluestack-ui/themed'
 
 export function Home() {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [groupSelected, setGroupSelected] = useState('Costas')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Group name="Costas" isActive={false} />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item: string) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
     </VStack>
   )
 }
