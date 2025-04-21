07ec49aab942b03146dc7c7d27168a0b1c0ac445
= <daniell.felipe.2017@gmail.com>
2025-04-20 22:09:39 -0300

    add password inputs

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,8 @@
 import { ScrollView, TouchableOpacity } from 'react-native'
 
-import { Center, Text, VStack } from '@gluestack-ui/themed'
+import { Center, Heading, Text, VStack } from '@gluestack-ui/themed'
 
+import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
@@ -34,6 +35,28 @@ export function Profile() {
           <Center w="$full" gap="$4">
             <Input placeholder="Nome" bg="$gray600" />
             <Input value="john@doe.com" bg="$gray600" isReadOnly />
+          </Center>
+
+          <Heading
+            alignSelf="flex-start"
+            fontFamily="$heading"
+            color="$gray200"
+            fontSize="$md"
+            mt="$12"
+            mb="$2"
+          >
+            Alterar senha
+          </Heading>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+            <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+            <Input
+              placeholder="Confirme a nova senha"
+              bg="$gray600"
+              secureTextEntry
+            />
+            <Button title="Atualizar" />
           </Center>
         </Center>
       </ScrollView>
