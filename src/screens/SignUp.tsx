68fcca25d0f337025a297fe68a17f01058da2807
= <daniell.felipe.2017@gmail.com>
2025-05-03 00:03:35 -0300

    add validation for name field in SignUp form and update dependencies

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -24,7 +24,11 @@ type FormDataProps = {
 }
 
 export function SignUp() {
-  const { control, handleSubmit } = useForm<FormDataProps>()
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>()
 
   const navigation = useNavigation<AuthNavigatorRoutesProps>()
 
@@ -70,6 +74,9 @@ export function SignUp() {
             <Controller
               control={control}
               name="name"
+              rules={{
+                required: 'Informe o nome.',
+              }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="Nome"
@@ -78,6 +85,10 @@ export function SignUp() {
                 />
               )}
             />
+
+            {errors.name?.message && (
+              <Text color="$white">{errors.name.message}</Text>
+            )}
 
             <Controller
               control={control}
