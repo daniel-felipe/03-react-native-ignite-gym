94bdc15f320662a075631e435f86ecd49f901598
= <daniell.felipe.2017@gmail.com>
2025-05-06 22:16:18 -0300

    add useAuth hook for simplified access to AuthContext in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,5 @@
-import { useContext } from 'react'
-
-import { AuthContext } from '@contexts/AuthContext'
 import { Box } from '@gluestack-ui/themed'
+import { useAuth } from '@hooks/useAuth'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 
 import { gluestackUIConfig } from '../../config/gluestack-ui.config'
@@ -9,8 +7,9 @@ import { AuthRoutes } from './auth.routes'
 // import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
-  const contextData = useContext(AuthContext)
-  console.log(contextData)
+  const { user } = useAuth()
+
+  console.log('Logged user: ', user)
 
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
