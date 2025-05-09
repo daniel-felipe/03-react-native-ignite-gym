012e880a184457f2b1e1db195ad9afd55c08b22c
= <daniell.felipe.2017@gmail.com>
2025-05-08 22:25:11 -0300

    update signIn function to handle asynchronous API calls and adjust user state management

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,11 @@
 import { ReactNode, createContext, useState } from 'react'
 
 import { UserDTO } from '@dtos/UserDTO'
+import { api } from '@services/api'
 
 export type AuthContextDataProps = {
   user: UserDTO
-  signIn: (email: string, password: string) => void
+  signIn: (email: string, password: string) => Promise<void>
 }
 
 type AuthContextProviderProps = {
@@ -16,20 +17,14 @@ export const AuthContext = createContext<AuthContextDataProps>(
 )
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-  const [user, setUser] = useState({
-    id: '1',
-    name: 'John Doe',
-    email: 'john@doe.com',
-    avatar: 'john-doe.png',
-  })
+  const [user, setUser] = useState<UserDTO>({} as UserDTO)
 
-  function signIn(email: string, password: string) {
-    setUser({
-      id: '',
-      name: '',
-      email,
-      avatar: '',
-    })
+  async function signIn(email: string, password: string) {
+    const { data } = await api.post('/sessions', { email, password })
+
+    if (data.user) {
+      setUser(data.user)
+    }
   }
 
   return (
