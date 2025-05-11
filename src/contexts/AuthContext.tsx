5f25d0e9fe7c4c7e0f2e68ac42b25723e9fb2bb0
= <daniell.felipe.2017@gmail.com>
2025-05-11 00:08:18 -0300

    add token removal in signOut function and implement storageAuthTokenRemove

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -4,6 +4,7 @@ import { UserDTO } from '@dtos/UserDTO'
 import { api } from '@services/api'
 import {
   storageAuthTokenGet,
+  storageAuthTokenRemove,
   storageAuthTokenSave,
 } from '@storage/storageAuthToken'
 import {
@@ -69,8 +70,10 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   async function signOut() {
     try {
       setIsLoadingUserStorageData(true)
+
       setUser({} as UserDTO)
       await storageUserRemove()
+      await storageAuthTokenRemove()
     } catch (error) {
       console.error(error)
     } finally {
