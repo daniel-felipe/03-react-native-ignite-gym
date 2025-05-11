10a5e86f62b3e040eebc2ccffd4a9645e83af807
= <daniell.felipe.2017@gmail.com>
2025-05-10 21:44:49 -0300

    add user storage functionality to persist user data in AsyncStorage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { ReactNode, createContext, useState } from 'react'
 
 import { UserDTO } from '@dtos/UserDTO'
 import { api } from '@services/api'
+import { storageUserSave } from '@storage/storageUser'
 
 export type AuthContextDataProps = {
   user: UserDTO
@@ -24,6 +25,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
     if (data.user) {
       setUser(data.user)
+      storageUserSave(data.user)
     }
   }
 
