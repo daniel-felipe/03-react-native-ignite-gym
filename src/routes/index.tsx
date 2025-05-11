d2f1009a72a34221adc7d52d35c7ae17fcc1181e
= <daniell.felipe.2017@gmail.com>
2025-05-10 21:38:23 -0300

    update routing logic to conditionally render app or auth routes based on user authentication status

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,13 +3,11 @@ import { useAuth } from '@hooks/useAuth'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 
 import { gluestackUIConfig } from '../../config/gluestack-ui.config'
+import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
-// import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
   const { user } = useAuth()
-
-  console.log('Logged user: ', user)
 
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
@@ -17,7 +15,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   )
