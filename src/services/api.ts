f3730096ba01947e0013ad6d6a9fb8ceec530336
= <daniell.felipe.2017@gmail.com>
2025-05-18 18:24:26 -0300

    add token management interceptor to API service and integrate sign-out handling

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,33 @@
+import axios, { AxiosInstance } from 'axios'
+
 import { AppError } from '@utils/AppError'
-import axios from 'axios'
+
+type SignOut = () => void
+
+type APIInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void
+}
 
 export const api = axios.create({
   baseURL: 'http://192.168.18.11:3333',
-})
+}) as APIInstanceProps
+
+api.registerInterceptTokenManager = (signOut) => {
+  const interceptTokenManager = api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response?.data) {
+        return Promise.reject(new AppError(error.response.data.message))
+      }
+
+      return Promise.reject(error)
+    }
+  )
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager)
+  }
+}
 
 api.interceptors.response.use(
   (response) => response,
